a6e3f5dde43fb56d1cf1a3f2b05e51d3d7e8c41b
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2013-07-04 14:58:25 +0200

    init

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,6 @@
+# Be sure to restart your server when you modify this file.
+
+Neo4jDemoApp::Application.config.session_store :cookie_store, key: '_neo4j-demo-app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
